f74f8cdb31228adca27b0a3f7d920ec9269a638e
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-11-14 00:23:01 +0000

    Populated other CLI commands

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@ from typing import List, Callable, overload
 import os
 from tubemap.tubemap_graph import TubemapGraph, SerializedTubemapGraph
 from tubemap.tubemap_node import TubemapNode
+from algorithms.graph_searcher import GraphSearcher
+from algorithms.dijkstras_algorithm import DijkstrasAlgorithm
 
 class Program:
     INFO = {
@@ -41,10 +43,10 @@ class Program:
             # "help",
             "list": Program.__command_list,
             "line": Program.__command_line,
-            "from": Program.__command_from,
-            "to": Program.__command_to,
+            "start": Program.__command_start,
+            "end": Program.__command_end,
             "algorithm": Program.__command_algorithm,
-            "calculate": Program.__command_calculate,
+            "go": Program.__command_go,
             "exit": Program.__command_exit
         }
 
@@ -74,7 +76,7 @@ class Program:
 
             if command_prefix == "help":
                 if len(command_args) == 0:
-                    print("Command syntax: <required> [optional]")
+                    print("Command syntax: [required] <optional>")
                     print(f"For help with sub-commands, type: help [command]")
                     print("Commands:")
                     for command in COMMANDS:
@@ -99,11 +101,11 @@ class Program:
         buffer: List[(str, str)] = []
 
         for node in Program.__graph.nodes.values():
-            info = [f"{node.label if node.label != '' else node.id}", ""]
+            info = [Program.__get_node_tag(node), ""]
 
             line_buffer = []
             for edge in node.adjacency_list:
-                line_buffer.append(f"{edge.label if edge.label != '' else edge.id}")
+                line_buffer.append(Program.__get_node_tag(edge))
             line_buffer.sort()
             info[1] = f"{', '.join(line_buffer)}"
 
@@ -141,59 +143,128 @@ class Program:
             print("Invalid second station.")
             return
 
-        line1_label = node1.label if node1.label != "" else node1.id
-        line2_label = node2.label if node2.label != "" else node2.id
-
-        edge = node1.adjacency_list.get(node2.id)
+        edge = node1.adjacency_list.get(node2)
         if edge is None:
             print("Stations do not have a line between them.")
             return
 
         if args[0] == "info":
-            print(f"Line between {line1_label} and {line2_label} is {'closed' if edge.closed else 'open'}.")
+            print(f"Line between '{Program.__get_node_tag(node1)}' and '{Program.__get_node_tag(node2)}' is {'closed' if edge.closed else 'open'}.")
         elif args[0] == "open":
             edge.closed = False
-            # print(f"Line between {line1_label} and {line2_label} is now open.")
+            print(f"Line between '{Program.__get_node_tag(node1)}' and '{Program.__get_node_tag(node2)}' is now open.")
         elif args[0] == "close":
             edge.closed = True
-            # print(f"Line between {line1_label} and {line2_label} is now closed.")
+            print(f"Line between '{Program.__get_node_tag(node1)}' and '{Program.__get_node_tag(node2)}' is now closed.")
         else:
             print("Invalid syntax.")
 
     @staticmethod
-    def __command_from(args: List[str], show_help = False) -> None:
+    def __command_start(args: List[str], show_help = False) -> None:
         """Sets the start node."""
         if show_help:
-            print("Sets the start node.")
-            print("Usage: from <node>")
+            print("Sets the station to start at.")
+            print("Usage:")
+            print("start\n\tShows the current start station.")
+            print("start [station]\n\tSets the start station.")
             return
 
+        if len(args) < 1:
+            if Program.__start_node is None:
+                print("The start station has not been set.")
+            else:
+                print(f"The start station is '{Program.__get_node_tag(Program.__start_node)}'.")
+            return
+
+        node = Program.__get_node_from_label_or_id(args[0])
+        if node is None:
+            print("Invalid station.")
+            return
+
+        Program.__start_node = node
+        print(f"Start station set to '{Program.__get_node_tag(node)}'.")
+
     @staticmethod
-    def __command_to(args: List[str], show_help = False) -> None:
+    def __command_end(args: List[str], show_help = False) -> None:
         """Sets the end node."""
         if show_help:
-            print("Sets the end node.")
-            print("Usage: to <node>")
+            print("Sets the station to end at.")
+            print("Usage:")
+            print("end\n\tShows the current end station.")
+            print("end [station]\n\tSets the end station.")
             return
+
+        if len(args) < 1:
+            if Program.__end_node is None:
+                print("The end station has not been set.")
+            else:
+                print(f"The end station is '{Program.__get_node_tag(Program.__end_node)}'.")
+            return
+
+        node = Program.__get_node_from_label_or_id(args[0])
+        if node is None:
+            print("Invalid station.")
+            return
+
+        Program.__end_node = node
+        print(f"End station set to '{Program.__get_node_tag(node)}'.")
 
     @staticmethod
     def __command_algorithm(args: List[str], show_help = False) -> None:
         """Sets the algorithm to use."""
         if show_help:
             print("Sets the algorithm to use.")
-            print("Usage: algorithm <algorithm>")
+            print("Usage:")
+            print("algorithm\n\tShows the current algorithm.")
+            print("algorithm [algorithm]\n\tSets the algorithm.")
             print("Algorithms:")
             for algorithm in Program.__ALGORITHMS:
-                print(f"{algorithm}")
+                print(f"\t{algorithm}")
             return
 
-    @staticmethod
-    def __command_calculate(args: List[str], show_help = False) -> None:
-        """Calculates the shortest path between the start and end nodes."""
-        if show_help:
-            print("Calculates the shortest path between the start and end nodes.")
-            print("Usage: calculate")
+        if len(args) < 1:
+            print(f"The current algorithm is '{Program.__ALGORITHMS[Program.__algorithm]}'.")
             return
+
+        if args[0].lower() not in Program.__ALGORITHMS:
+            print("Invalid algorithm.")
+            return
+
+        Program.__algorithm = args.index(args[0].lower())
+        print(f"Algorithm set to '{Program.__ALGORITHMS[Program.__algorithm]}'.")
+
+    @staticmethod
+    def __command_go(args: List[str], show_help = False) -> None:
+        """Finds the shortest path between the set start and end nodes using the specified algorithm."""
+        if show_help:
+            print("Finds the shortest path between the set start and end nodes using the specified algorithm.")
+            print("Usage: calculate")
+
+        if Program.__start_node is None:
+            print("The start station has not been set.")
+            return
+        elif Program.__end_node is None:
+            print("The end station has not been set.")
+            return
+
+        if not GraphSearcher.is_path_available(Program.__start_node, Program.__end_node, True):
+            print("No path is available between the start and end stations.")
+            return
+
+        weight = 0
+        node_array = []
+
+        if Program.__algorithm == 0:
+            dijkstra_node = DijkstrasAlgorithm.find_shortest_path(Program.__graph, Program.__start_node, Program.__end_node)
+            weight = dijkstra_node.path_weight
+            node_array = DijkstrasAlgorithm.dijkstra_node_to_node_array(dijkstra_node)
+
+        buffer = []
+        for node in node_array:
+            buffer.append(Program.__get_node_tag(node))
+        path_string = " -> ".join(buffer)
+
+        print(f"The shortest path from '{Program.__get_node_tag(Program.__start_node)}' to '{Program.__get_node_tag(Program.__end_node)}', with a duration of {weight} minutes, is: {path_string}")
 
     @staticmethod
     def __command_exit(args: List[str], show_help = False) -> None:
@@ -216,6 +287,11 @@ class Program:
     def __get_node_from_label_or_id(tag: str) -> TubemapNode:
         """Finds the first node in a graph matching against a label or ID."""
         return Program.__get_node(lambda node: node.label.lower() == tag.lower() or node.id == tag)
+
+    @staticmethod
+    def __get_node_tag(node: TubemapNode) -> str:
+        """Gets the tag of a node."""
+        return (node.label if node.label != "" else node.id).strip()
 
 if __name__ == "__main__":
     Program.Main()
