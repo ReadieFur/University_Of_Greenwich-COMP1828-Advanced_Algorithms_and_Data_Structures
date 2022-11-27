6904d8b5fd2bd408ae99fb3bfbf7def4464f590f
Tristan Read <60425965+ReadieFur@users.noreply.github.com>
2022-11-27 19:14:38 +0000

    Update submission scripts

--- submission.cmd
+++ submission.cmd
@@ -1,1 +1,7 @@
-7z.exe a -t7z .\submission.7z .\dist.7z .\report.pdf
+@REM Archive the src folder to .\submission\COMP1828_001151378_SRC.zip file
+@REM https://superuser.com/questions/97342/7zip-command-line-exclude-folders-by-wildcard-pattern
+@REM https://superuser.com/questions/340046/creating-an-archive-from-a-directory-without-the-directory-name-being-added-to-t
+7z.exe a .\submission\COMP1828_001151378_SRC.zip .\src -mx0 -xr!__pycache__ -xr!*.map
+
+@REM Copy the report.pdf to the submission folder.
+move .\report.pdf .\submission\COMP1828_001151378_REPORT.pdf
